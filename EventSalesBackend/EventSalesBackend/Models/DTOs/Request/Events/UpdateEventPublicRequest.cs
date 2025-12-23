a9e2ef5b4227db2446bf9a9b60e260cb97daefae
samsonswinburne <161039641+samsonswinburne@users.noreply.github.com>
2025-12-23 18:41:39 +1100

    Add simple update event public request DTO and add validator

---
+++ EventSalesBackend/EventSalesBackend/Models/DTOs/Request/Events/UpdateEventPublicRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace EventSalesBackend.Models.DTOs.Request.Events;
+
+public class UpdateEventPublicRequest
+{
+    public required string EventId { get; set; }
+}
---
+++ EventSalesBackend/EventSalesBackend/Validators/Events/UpdateEventPublicRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using EventSalesBackend.Models.DTOs.Request.Events;
+using FluentValidation;
+using MongoDB.Bson;
+
+namespace EventSalesBackend.Validators.Events;
+
+public class UpdateEventPublicRequestValidator : AbstractValidator<UpdateEventPublicRequest>
+{
+    public UpdateEventPublicRequestValidator()
+    {
+        RuleFor(x => x.EventId).NotEmpty().WithMessage("EventId cannot be empty")
+            .Length(24).WithMessage("EventId must be 24 characters")
+            .Must(id => ObjectId.TryParse(id, out _)).WithMessage("EventId must be a valid eventId");
+    }
+}
