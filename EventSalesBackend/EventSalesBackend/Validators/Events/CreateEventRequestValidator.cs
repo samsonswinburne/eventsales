f680d4c8814e18eba3df259528a9acbf84a40228
samsonkemp <161039641+samsonswinburne@users.noreply.github.com>
2025-12-30 17:30:44 +1100

    again more fixing pattern class name change errors from merge

--- EventSalesBackend/EventSalesBackend/Validators/Events/CreateEventRequestValidator.cs
+++ EventSalesBackend/EventSalesBackend/Validators/Events/CreateEventRequestValidator.cs
@@ -15,11 +15,11 @@ public class CreateEventRequestValidator : AbstractValidator<CreateEventRequest>
         RuleFor(x => x.Name)
             .NotEmpty().WithMessage("Event name is required")
             .Length(3, 200).WithMessage("Event name must be between 3 and 200 characters")
-            .Matches(RegexPatterns.NamePattern).WithMessage(RegexPatterns.NamePatternMessage);
+            .Matches(RegexPatterns.NamePattern).WithMessage(RegexValidationMessages.NamePatternMessage);
 
         RuleFor(x => x.Description)
             .MaximumLength(2000).WithMessage("Description cannot exceed 2000 characters")
-            .Matches(RegexPatterns.NamePattern).WithMessage(RegexPatterns.NamePatternMessage);
+            .Matches(RegexPatterns.NamePattern).WithMessage(RegexValidationMessages.NamePatternMessage);
 
         RuleFor(x => x.StartDate)
             .NotEmpty().WithMessage("Start date is required")
