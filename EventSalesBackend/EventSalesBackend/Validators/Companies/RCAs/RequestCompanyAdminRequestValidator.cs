1df84f1cc703cd11b3c7e70555686a033c585a42
samsonswinburne <161039641+samsonswinburne@users.noreply.github.com>
2026-01-02 22:36:58 +1100

    Add Simple validator for RequestCompanyAdminRequest
    Checks that it isn't empty and is in the format of an email

---
+++ EventSalesBackend/EventSalesBackend/Validators/Companies/RCAs/RequestCompanyAdminRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using EventSalesBackend.Models;
+using EventSalesBackend.Models.DTOs.Request.Companies;
+using FluentValidation;
+
+namespace EventSalesBackend.Validators.Companies.RCAs;
+
+public class RequestCompanyAdminRequestValidator : AbstractValidator<RequestCompanyAdminRequest>
+{
+    public RequestCompanyAdminRequestValidator()
+    {
+        RuleFor(x => x.AdminRequestReceiverEmail)
+            .NotEmpty()
+            .EmailAddress();
+    }
+}
