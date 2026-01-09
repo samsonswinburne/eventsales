91e839aaa0c4c02c10cf28eb28177ea29a0f82b0
samsonkemp <161039641+samsonswinburne@users.noreply.github.com>
2026-01-09 11:22:39 +1000

    Add simple request DTO and validator for remove company admin

---
+++ EventSalesBackend/EventSalesBackend/Models/DTOs/Request/Companies/RemoveCompanyAdminRequest.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace EventSalesBackend.Models.DTOs.Request.Companies
+{
+    public class RemoveCompanyAdminRequest
+    {
+        public required string userId { get; init; }
+    }
+}
---
+++ EventSalesBackend/EventSalesBackend/Validators/Companies/RemoveCompanyAdminRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using EventSalesBackend.Models.DTOs.Request.Companies;
+using FluentValidation;
+using MongoDB.Bson;
+
+namespace EventSalesBackend.Validators.Companies
+{
+    public class RemoveCompanyAdminRequestValidator : AbstractValidator<RemoveCompanyAdminRequest>
+    {
+        public RemoveCompanyAdminRequestValidator()
+        {
+            RuleFor(x => x.userId)
+                .NotEmpty().WithMessage("UserId to remove must not be empty");
+                
+        }
+    }
+}
