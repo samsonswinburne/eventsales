8ac31bef53eab361e30cc68462dbb760147a4b14
samsonkemp <161039641+samsonswinburne@users.noreply.github.com>
2026-01-27 16:44:07 +1100

    Add ITicketRepository interface
    Introduces the ITicketRepository interface with methods for retrieving and inserting Ticket objects. This provides an abstraction for ticket data operations.

---
+++ EventSalesBackend/EventSalesBackend/Repositories/Interfaces/ITicketRepository.cs
@@ -0,0 +1,11 @@
+﻿using EventSalesBackend.Models;
+using MongoDB.Bson;
+
+namespace EventSalesBackend.Repositories.Interfaces
+{
+    public interface ITicketRepository
+    {
+        public Task<Ticket?> Get(ObjectId id);
+        public Task<bool> Insert(Ticket ticket);
+    }
+}
